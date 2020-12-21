b8f78b46d768cbea5e0f0fabfe99cc4f7911d4a9
moha6248 <51365122+moha6248@users.noreply.github.com>
2020-12-21 15:38:30 +0100

    Delete Mapping

--- programmering-eksam2/src/main/java/com/example/demo/controller/HomeController.java
+++ programmering-eksam2/src/main/java/com/example/demo/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Student;
 import com.example.demo.repository.IStudentRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +34,16 @@ public class HomeController {
         }else{
             return ResponseEntity.status(404).body(response);
         }
+    }
+
+
+
+
+    //delete by id
+
+    @DeleteMapping("/deleteStudent/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        iStudentRepository.deleteById(id);
+        return ResponseEntity.status(200).body("{'msg' : 'deleted'}");
     }
 }
