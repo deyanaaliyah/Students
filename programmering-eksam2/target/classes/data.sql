562d21189474ec86c8ba9a1653c61ad2f7fa30ff
Mugisha98 <58075884+Mugisha98@users.noreply.github.com>
2020-12-21 14:54:33 +0100

    dataSql

--- programmering-eksam2/target/classes/data.sql
+++ programmering-eksam2/target/classes/data.sql
@@ -1,7 +1,3 @@
-
-
-
-
 INSERT INTO Supervisors
 VALUES (1,'Betty', 'Nayz','bn@kea.dk'),
        (2,'Tamanna', 'Clements','tc@kea.dk'),
@@ -14,14 +10,12 @@ VALUES (1,'Betty', 'Nayz','bn@kea.dk'),
        (9,'Sally', 'Dillon','sd@kea.dk'),
        (10,'Montague', 'Mcconnell','mm@kea.dk'),
        (11,'Douglas', 'Sosa','dd@kea.dk');
-
-
 INSERT INTO Students
-VALUES (1,'Layla', 'Fenster','layl2014@stud.kea.dk',1)
-      /* (2,'Noah','Jackson','noah0149@stud.kea.dk'),
-       (3,'Hugh','Jass','hugh@stud.kea.dk'),
-       (4,'Gabe','Itch','gabe@stud.kea.dk'),
-       (5,'Jeniffer','Mohde','jenni@stud.kea.dk'),
-       (6,'Cardi','Offset','bardi@stud.kea.dk'),
-       (7,'Nicki','Minaj','nicki@stud.kea.dk'),
-       (8,'Douglas','Jew','doug@stud.kea.dk')*/;
+VALUES (1,'Layla', 'Fenster','layl2014@stud.kea.dk',1),
+       (2,'Noah','Jackson','noah0149@stud.kea.dk',1),
+       (3,'Hugh','Jass','hugh@stud.kea.dk',1),
+       (4,'Gabe','Itch','gabe@stud.kea.dk',1),
+       (5,'Jeniffer','Mohde','jenni@stud.kea.dk',1),
+       (6,'Cardi','Offset','bardi@stud.kea.dk',1),
+       (7,'Nicki','Minaj','nicki@stud.kea.dk',1),
+       (8,'Douglas','Jew','doug@stud.kea.dk',1);
