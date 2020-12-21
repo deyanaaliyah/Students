42e67fa6575fd560e9c0b83a856838e7c8959cfa
moha6248 <51365122+moha6248@users.noreply.github.com>
2020-12-21 14:49:31 +0100

    sql

--- programmering-eksam2/src/main/resources/data.sql
+++ programmering-eksam2/src/main/resources/data.sql
@@ -1,39 +1,4 @@
--- TABLES/DATA TO BE ADDED
-CREATE TABLE IF NOT EXISTS Test (
-                                    id INT(100) NOT NULL auto_increment,
-                                    car_type VARCHAR(200),
-                                    first_name VARCHAR(200),
-                                    last_name VARCHAR(200),
-                                    mail VARCHAR(200),
-                                    phone INT(8),
-                                    PRIMARY KEY (id));
-CREATE TABLE IF NOT EXISTS Students(
-                                       id INT(100) NOT NULL auto_increment,
-                                       first_name VARCHAR(200),
-                                       last_name VARCHAR(200),
-                                       email VARCHAR(200),
-                                       supervisor_id INT NOT null,
-                                       PRIMARY KEY (id));
-                                       -- ALTER TABLE Students ADD FOREIGN KEY (supervisor_id) references Supervisors(id);
-                                       -- FOREIGN KEY (supervisor_id) REFERENCES Supervisors(id));
-
-CREATE TABLE IF NOT EXISTS Supervisors(
-                                       id INT(100) NOT NULL auto_increment,
-                                       first_name VARCHAR(200),
-                                       last_name VARCHAR(200),
-                                       email VARCHAR(200),
-                                       PRIMARY KEY (id));
-
-INSERT INTO Students(id,first_name,last_name,email, supervisor_id)
-VALUES (1,'Layla', 'Fenster','layl2014@stud.kea.dk',1),
-       (2,'Noah','Jackson','noah0149@stud.kea.dk',1),
-       (3,'Hugh','Jass','hugh@stud.kea.dk',1),
-       (4,'Gabe','Itch','gabe@stud.kea.dk',1),
-       (5,'Jeniffer','Mohde','jenni@stud.kea.dk',1),
-       (6,'Cardi','Offset','bardi@stud.kea.dk',1),
-       (7,'Nicki','Minaj','nicki@stud.kea.dk',1),
-       (8,'Douglas','Jew','doug@stud.kea.dk',1);
-INSERT INTO Supervisors(id,first_name,last_name,email)
+INSERT INTO Supervisors
 VALUES (1,'Betty', 'Nayz','bn@kea.dk'),
        (2,'Tamanna', 'Clements','tc@kea.dk'),
        (3,'Conner', 'Cabrera','cc@kea.dk'),
@@ -45,3 +10,14 @@ VALUES (1,'Betty', 'Nayz','bn@kea.dk'),
        (9,'Sally', 'Dillon','sd@kea.dk'),
        (10,'Montague', 'Mcconnell','mm@kea.dk'),
        (11,'Douglas', 'Sosa','dd@kea.dk');
+
+
+INSERT INTO Students
+VALUES (1,'Layla', 'Fenster','layl2014@stud.kea.dk',1)
+      /* (2,'Noah','Jackson','noah0149@stud.kea.dk'),
+       (3,'Hugh','Jass','hugh@stud.kea.dk'),
+       (4,'Gabe','Itch','gabe@stud.kea.dk'),
+       (5,'Jeniffer','Mohde','jenni@stud.kea.dk'),
+       (6,'Cardi','Offset','bardi@stud.kea.dk'),
+       (7,'Nicki','Minaj','nicki@stud.kea.dk'),
+       (8,'Douglas','Jew','doug@stud.kea.dk')*/;
