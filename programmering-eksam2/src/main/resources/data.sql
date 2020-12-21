01303565badfc79f7050221e8ac91124c9507c2a
deyanaaliyah <62359528+deyanaaliyah@users.noreply.github.com>
2020-12-21 11:41:44 +0100

    lort

--- programmering-eksam2/src/main/resources/data.sql
+++ programmering-eksam2/src/main/resources/data.sql
@@ -1,1 +1,9 @@
 -- TABLES/DATA TO BE ADDED
+CREATE TABLE IF NOT EXISTS Customers (
+                                         id INT(100) NOT NULL auto_increment,
+                                         car_type VARCHAR(200),
+                                         first_name VARCHAR(200),
+                                         last_name VARCHAR(200),
+                                         mail VARCHAR(200),
+                                         phone INT(8),
+                                         PRIMARY KEY (id));
