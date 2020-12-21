c704083c8ad4312b5917bb0b426b81e136f5d58e
deyanaaliyah <62359528+deyanaaliyah@users.noreply.github.com>
2020-12-21 16:48:59 +0100

    delete + read all

--- programmering-eksam2/src/main/java/com/example/demo/controller/StudentController.java
+++ programmering-eksam2/src/main/java/com/example/demo/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Student;
 import com.example.demo.repository.IStudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class StudentController {
@@ -19,4 +23,13 @@ public class StudentController {
     }
 
 
+    @GetMapping("/delete-student/{id}")
+    private String deleteAlbum(@PathVariable("id") Long id){
+
+        Optional<Student> student = repository.findById(id);
+
+        repository.deleteById(student.get().getId());
+
+        return "successful";
+    }
 }
