f75b1dfe8f783708a7ae27c3946ba1663a9c9560
deyanaaliyah <62359528+deyanaaliyah@users.noreply.github.com>
2020-12-21 10:48:03 +0100

    test

---
+++ src/main/java/kea/exam/demo/ProgrammeringApplication.java
@@ -0,0 +1,13 @@
+package kea.exam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammeringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammeringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/exam/demo/ProgrammeringApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.exam.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammeringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
