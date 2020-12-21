f61dd72c35158f00a00652b69e33b0f530b90675
Mugisha98 <58075884+Mugisha98@users.noreply.github.com>
2020-12-21 11:36:14 +0100

    fail

--- programmering-eksam2/src/main/java/com/example/demo/model/Student.java
+++ programmering-eksam2/src/main/java/com/example/demo/model/Student.java
@@ -1,4 +1,66 @@
 package com.example.demo.model;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import org.hibernate.property.access.spi.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @ManyToOne
+    private Supervisor supervisor;
+
+    //Getter
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Supervisor getSupervisor() {
+        return supervisor;
+    }
+
+    //Setter
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSupervisor(Supervisor supervisor) {
+        this.supervisor = supervisor;
+    }
 }
--- programmering-eksam2/src/main/java/com/example/demo/model/Supervisor.java
+++ programmering-eksam2/src/main/java/com/example/demo/model/Supervisor.java
@@ -1,4 +1,63 @@
 package com.example.demo.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Supervisors")
 public class Supervisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @OneToMany(mappedBy = "supervisor")
+    private List<Student> studentList;
+
+    //Getter
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    //Setter
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
 }
